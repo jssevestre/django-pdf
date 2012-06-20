db22cfa8f65dcafeaef92e7d35ee11bbaeb16606
jssevestre <contact@jssevestre.net>
2012-06-20 23:03:03 +0200

    Update to xhtml2pdf 0.0.4 Remove unused var Remove used of predefined var (format)

--- django_pdf/context_processors.py
+++ django_pdf/context_processors.py
@@ -2,9 +2,7 @@ from django.conf import settings
 
 REQUEST_FORMAT_NAME = getattr(settings, 'REQUEST_FORMAT_NAME', 'format')
 REQUEST_FORMAT_PDF_VALUE = getattr(settings, 'REQUEST_FORMAT_PDF_VALUE', 'pdf')
-TEMPLATE_PDF_CHECK = getattr(settings, 'TEMPLATE_PDF_CHECK',
-'DJANGO_PDF_OUTPUT')
-
+TEMPLATE_PDF_CHECK = getattr(settings, 'TEMPLATE_PDF_CHECK','DJANGO_PDF_OUTPUT')
 
 def check_format(request):
     """
@@ -22,8 +20,8 @@ def check_format(request):
     Here the value of TEMPLATE_PDF_CHECK settings var is the default one, i.e.
     DJANGO_PDF_OUTPUT. You can change this in your settings
     """
-    format = request.GET.get(REQUEST_FORMAT_NAME, None)
-    if format == REQUEST_FORMAT_PDF_VALUE:
+    reqformat = request.GET.get(REQUEST_FORMAT_NAME, None)
+    if reqformat == REQUEST_FORMAT_PDF_VALUE:
         return {TEMPLATE_PDF_CHECK: True}
     else:
         return {TEMPLATE_PDF_CHECK: False}
--- django_pdf/middleware.py
+++ django_pdf/middleware.py
@@ -1,49 +1,60 @@
 #!/usr/bin/env python
-
+# TODO : pdf generation consume heavy ressources -> store pdf in media path and
+#       test about age|modification before render or give from cache
 import os.path
+from xhtml2pdf import pisa
 import cStringIO as StringIO
-import cgi
-import ho.pisa as pisa
-import cStringIO as StringIO
-import cgi
 from django.http import HttpResponse
 from django.conf import settings
+import re
 
 REQUEST_FORMAT_NAME = getattr(settings, 'REQUEST_FORMAT_NAME', 'format')
 REQUEST_FORMAT_PDF_VALUE = getattr(settings, 'REQUEST_FORMAT_PDF_VALUE', 'pdf')
-TEMPLATE_PDF_CHECK = getattr(settings, 'TEMPLATE_PDF_CHECK',
-'DJANGO_PDF_OUTPUT')
+TEMPLATE_PDF_CHECK = getattr(settings, 'TEMPLATE_PDF_CHECK','DJANGO_PDF_OUTPUT')
+PDF_BASE_NAME =  getattr(settings, 'PDF_BASE_NAME','Mysite')
 
 
 def fetch_resources(uri, rel):
     """
     Prepares paths for pisa
+    Remember : you fetch for 'printing' so create a 'media=print' css import in your template
+    # TODO test on server ( without ./manage.py runserver )
     """
-    path = os.path.join(settings.MEDIA_ROOT,
-            uri.replace(settings.MEDIA_URL, ""))
+    if re.match(".*%s.*"%settings.MEDIA_URL,uri):
+	path = os.path.join(settings.MEDIA_ROOT,uri.replace(settings.MEDIA_URL, ""))
+
+    if re.match(".*%s.*"%settings.STATIC_URL,uri):
+	path = os.path.join(settings.STATIC_ROOT,uri.replace(settings.STATIC_URL, ""))
     return path
 
 
-def transform_to_pdf(response):
+def transform_to_pdf(response,pdfname):
+    """
+    call xhtml2pdf.pisa to convert html responce to pdf
+    """
     response['mimetype'] = 'application/pdf'
-    response['Content-Disposition'] = 'attachment; filename=report.pdf'
+
+    # TODO : on the fly filename from url
+    response['Content-Disposition'] = 'attachment; filename=%s.pdf' % pdfname
+
     content = response.content
-    result = StringIO.StringIO()
     pdf = pisa.pisaDocument(StringIO.StringIO(content),
             response, link_callback=fetch_resources)
+
     if not pdf.err:
         return response
     else:
-        return http.HttpResponse('We had some errors<pre>%s</pre>' %
-                cgi.escape(html))
-
+        # TODO return error and redirect to default view
+        return HttpResponse('We had some errors in pdfMiddleWare : \
+                            <br/><pre>%s</pre>'%pdf)
 
 class PdfMiddleware(object):
     """
-    Converts the response to a pdf one.
+    Converts the response to a pdf one if requested by '?format=pdf'
     """
     def process_response(self, request, response):
-        format = request.GET.get(REQUEST_FORMAT_NAME, None)
-        if format == REQUEST_FORMAT_PDF_VALUE:
-            response = transform_to_pdf(response)
+        reqformat = request.GET.get(REQUEST_FORMAT_NAME, None)
+        if reqformat == REQUEST_FORMAT_PDF_VALUE:
+	    pdfname = '%s%s'%(PDF_BASE_NAME, request.path.replace('/','_'))
+            response = transform_to_pdf(response,pdfname)
         return response
